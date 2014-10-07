e6ebb83f4ac6ee1be0c9c2d2fe13ea7c4a639d9d
Tomas Nagy <Tomas@Tomass-MacBook-Pro.local>
2014-10-07 12:41:38 +0200

    Week 3 working, needs style

---
+++ Week 3/Oef1/js/script.js
@@ -0,0 +1,31 @@
+(function init() {
+    var button = document.getElementsByTagName("button")[0];
+    button.addEventListener("click", buttonClicked);
+})();
+
+function buttonClicked(e) {
+    e.preventDefault();
+    var fname = document.getElementById("firstname"),
+        lname = document.getElementById("lastname"),
+        age = document.getElementById("age"),
+        image = document.getElementById("image"),
+        htmlBuilder,
+        section = document.getElementsByTagName("section")[0];
+
+    htmlBuilder = '<figure data-age="' + age.value + '"><img src="' + image.value + '" alt= "person"/><figcaption>' + fname.value + ' ' + lastname.value + '</figcaption></figure>';
+
+
+    section.innerHTML += htmlBuilder;
+
+    checkAge();
+}
+
+function checkAge() {
+    var el = document.querySelector("figure:last-child"), age = el.getAttribute("data-age");
+
+    if(age < 18) {
+        el.setAttribute("class", "young");
+    } else {
+        el.setAttribute("class", "old");
+    }
+}
